1094c302dc04144ac16a36d59f18351338e178b7
Rizwan Rasheed <Rizwan@Rizwan-Rasheed-TCE.local>
2021-10-22 15:28:16 +0500

    First commit

---
+++ tarball/gatherer/karma.conf.js
@@ -0,0 +1,30 @@
+
+module.exports = function(config) {
+  config.set({
+    basePath: "",
+    frameworks: ["jasmine"],
+    files: ["spec/javascripts/**/*_spec.js"],
+    preprocessors: {
+      "app/javascript/packs/*.js": ["webpack", "sourcemap"],
+      "spec/javascripts/**/*_spec.js": ["webpack", "sourcemap"],
+    },
+    reporters: ["mocha"],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--headless', '--disable-gpu','--no-sandbox','--disable-dev-shm-usage', '--screenshot']
+      }
+    },
+    singleRun: true,
+    concurrency: Infinity,
+    webpack: require("./config/webpack/test.js"),
+    webpackMiddleware: {
+      stats: "errors-only"
+    }
+  })
+}
