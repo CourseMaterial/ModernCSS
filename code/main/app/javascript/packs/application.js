f3246d09f59ab7c3c2c7b9a80fd993146820f735
CourseMaterial <55927635+CourseMaterial@users.noreply.github.com>
2022-01-04 11:09:38 +0500

    Update application.js

--- code/main/app/javascript/packs/application.js
+++ code/main/app/javascript/packs/application.js
@@ -1,11 +1,4 @@
-/***
- * Excerpted from "Modern CSS with Tailwind",
- * published by The Pragmatic Bookshelf.
- * Copyrights apply to this code. It may not be used to create training material,
- * courses, books, articles, and the like. Contact us if you are in doubt.
- * We make no guarantees that this code is fit for any purpose.
- * Visit http://www.pragmaticprogrammer.com/titles/tailwind for more book information.
-***/
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
